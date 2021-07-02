c2cd8c3c21f8258adbb0cecfca3b755c6510d030
Alan Lo <alan.lo@servian.com>
2021-07-02 12:56:36 +1000

    chalk and fixed ignore

--- learn_node_udemy/notes-app/app.js
+++ learn_node_udemy/notes-app/app.js
@@ -1,12 +1,12 @@
 const fs = require('fs')
 
 const validator = require('validator')
+const chalk = require('chalk')
 
 const add = require('./utils.js')
 const getNotes = require('./notes.js')
 
 //fs.writeFileSync('notes.txt', 'hello')
-
 fs.appendFileSync('notes.txt', ' world')
 
 console.log(add(1, 1))
@@ -18,3 +18,6 @@ console.log(validator.isEmail('blah@blah.com'))
 console.log(validator.isEmail('blah.com'))
 console.log(validator.isURL('https://blah.com'))
 
+
+msg = chalk.bold.green('Chalk bold green')
+console.log(msg)
