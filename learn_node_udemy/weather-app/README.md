3b3f7dca61ec77d4c08f35e2316298c0433bd77d
Alan Lo <alan.lo@servian.com>
2021-07-02 16:01:13 +1000

    weather

---
+++ learn_node_udemy/weather-app/README.md
@@ -0,0 +1,5 @@
+Didn't want to register an account on weatherstack to do this tute. Just run simple python http and a test json
+
+```
+python3 -m http.server
+```
--- learn_node_udemy/weather-app/app.js
+++ learn_node_udemy/weather-app/app.js
@@ -1,4 +1,28 @@
+const request = require('request')
+
 console.log('Starting')
+
+// Don't forget to start the file server...see README
+const url = 'http://localhost:8000/test.json'
+
+request({ url: url, json: true}, (error, response) => {
+    if (error) {
+        console.log(error)
+        console.log(response)
+    }
+    else if (response.body.error) {
+        console.log('Params were wrong')
+    }
+    else {
+        console.log(response.body.currently)
+    }
+})
+
+/*
+setTimeout(() => {
+    console.log('2 second timer')
+}, 2000)
+*/
 
 
 console.log('Stopping')
