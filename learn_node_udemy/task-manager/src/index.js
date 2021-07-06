26a0f36d07d0452a24f885aa938728a0be4ab38b
Alan Lo <alan.lo@servian.com>
2021-07-06 10:56:53 +1000

    delete

--- learn_node_udemy/task-manager/src/index.js
+++ learn_node_udemy/task-manager/src/index.js
@@ -58,6 +58,45 @@ app.get('/users/:id', (req, res) => {
 })
 */
 
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates' })
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { 
+            new: true, 
+            runValidators: true
+        })
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.status(201).send(user)
+    } catch (e) {
+        console.log('Error', e)
+        res.status(400).send(e)
+    }
+})
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        console.log('Error', e)
+        res.status(500).send(e)
+    }
+})
+
 
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
@@ -93,6 +132,42 @@ app.get('/tasks/:id', async (req, res) => {
     }
 })
 
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates' })
+    }
+
+    try{
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        })
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.status(201).send(task)
+    } catch (e) {
+        console.log('Error', e)
+        res.status(400).send(e)
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch(e) {
+        console.log('Error', e)
+        res.status(500).send(e)
+    }
+})
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
