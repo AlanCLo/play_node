2b912387dff5e0e029c21ffb99c69de5b060df03
Alan Lo <alan.lo@servian.com>
2021-07-06 14:31:06 +1000

    auth

--- learn_node_udemy/task-manager/src/routers/user.js
+++ learn_node_udemy/task-manager/src/routers/user.js
@@ -2,19 +2,32 @@ const express = require('express')
 const router = new express.Router()
 
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({user, token})
     } catch (e) {
         console.log('Error', e)
         res.status(400).send(e)
     }
 })
 
-router.get('/users', async (req, res) => {
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({user, token})
+    } catch (e) {
+        console.log(e)
+        res.status(400).send()
+    }
+})
+
+router.get('/users', auth, async (req, res) => {
     try {
         users = await User.find()
         res.status(201).send(users)
